ae92525c5987b986bed7062cab9d7d95724a45f0
basjena <basjena@in-mum-mobibmx1.corp.capgemini.com>
2018-12-12 16:16:50 +0530

    Committing for Account java

--- BankingApplication/src/Account.java
+++ BankingApplication/src/Account.java
@@ -3,6 +3,13 @@ public class Account {
 	
 	private int accountNumber;
 	private int amount;
+	private int depositeAmount;
+	public int getDepositeAmount() {
+		return depositeAmount;
+	}
+	public void setDepositeAmount(int depositeAmount) {
+		this.depositeAmount = depositeAmount;
+	}
 	public Account(int accountNumber, int amount) {
 		super();
 		this.accountNumber = accountNumber;
